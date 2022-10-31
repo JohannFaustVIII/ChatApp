fd9c086f30bedb0e28ae0ab9f1701266ccfc5327
Faust VIII <joh.faustviii@gmail.com>
2022-10-31 13:40:58 +0100

    Add OpenApi classes generating

---
+++ backend/src/main/java/org/faust/chat/MessagesController.java
@@ -0,0 +1,9 @@
+package org.faust.chat;
+
+import org.faust.chat.api.MessagesApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessagesController implements MessagesApi {
+
+}
