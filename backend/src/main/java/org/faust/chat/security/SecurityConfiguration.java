5417dfab4cc66b8a16f3e4b21ce8053873d6c0e6
Faust VIII <joh.faustviii@gmail.com>
2022-12-01 12:53:56 +0100

    Next attempt to keycloak authentication (failure)

--- backend/src/main/java/org/faust/chat/security/SecurityConfiguration.java
+++ backend/src/main/java/org/faust/chat/security/SecurityConfiguration.java
@@ -2,13 +2,17 @@ package org.faust.chat.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -27,11 +31,12 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
-        http.authorizeExchange()
-                .pathMatchers("/messages*").hasRole("USER")
-                .anyExchange().permitAll();
-        http.oauth2Client().and().logout().logoutHandler(keycloakLogoutHandler).logoutUrl("/");
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http, Converter<Jwt, Mono<AbstractAuthenticationToken>> jwtAuthenticationConverter) {
+        http.authorizeExchange().anyExchange().authenticated().and().oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter);
+//        http.authorizeExchange()
+//                .pathMatchers("/messages*").authenticated()//.hasRole("USER")
+//                .anyExchange().permitAll();
+//        http.oauth2Client().and().logout().logoutHandler(keycloakLogoutHandler).logoutUrl("/");
         return http.build();
     }
 }
