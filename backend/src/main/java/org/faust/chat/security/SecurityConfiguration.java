6d87c09940ea6802d69a3edcf9f7e0027b9fc96a
Faust VIII <joh.faustviii@gmail.com>
2022-12-29 13:47:44 +0100

    Idea to solve problem with keycloak login

--- backend/src/main/java/org/faust/chat/security/SecurityConfiguration.java
+++ backend/src/main/java/org/faust/chat/security/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -24,6 +25,9 @@ public class SecurityConfiguration {
                 .authorizeExchange(exchanges -> exchanges
                         .anyExchange().authenticated()
                 )
+                .oauth2Login()
+                .authenticationMatcher(new PathPatternParserServerWebExchangeMatcher("/login2")) // to remove and give access to register and etc. instead?
+                .and()
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
                                 .jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
---
+++ backend/src/main/java/org/faust/chat/security/SecurityController.java
@@ -0,0 +1,14 @@
+package org.faust.chat.security;
+
+public class SecurityController {
+
+    /*
+    TODO:
+        1. Controller to login and register, maybe logout too?
+        2. Create models for login and register
+        3. Create annotations to verify e-mail and matching passwords (to use with Valid in controller)
+        4. Create interface for authenticating service
+        5. Create implementation of the service using Keycloak (with defaults etc.)
+        6. Update SecurityConfiguration (remove usage of login redirect to keycloak and add access to endpoints in the controller)
+     */
+}
