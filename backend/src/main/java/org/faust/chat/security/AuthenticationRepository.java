3b46f393e0e69a370224d54bd85069deae23dba1
Faust VIII <joh.faustviii@gmail.com>
2023-01-10 12:35:56 +0100

    Define simple authentication repository

--- backend/src/main/java/org/faust/chat/security/AuthenticationRepository.java
+++ backend/src/main/java/org/faust/chat/security/AuthenticationRepository.java
@@ -1,4 +1,13 @@
 package org.faust.chat.security;
 
 public interface AuthenticationRepository {
+
+    String authorize(String user, String password);
+
+    boolean isValid(String token);
+
+    String refresh(String token);
+
+    boolean logout(String token);
+
 }
--- backend/src/main/java/org/faust/chat/security/keycloak/KeycloakAuthenticationRepository.java
+++ backend/src/main/java/org/faust/chat/security/keycloak/KeycloakAuthenticationRepository.java
@@ -1,8 +1,44 @@
 package org.faust.chat.security.keycloak;
 
-import org.faust.chat.security.service.AuthenticationRepository;
-import org.springframework.stereotype.Component;
+import org.faust.chat.security.AuthenticationRepository;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class KeycloakAuthenticationRepository implements AuthenticationRepository {
+
+    // client registration - /clients-registrations/openid-connect ??? will it work for users
+
+    @Override
+    public String authorize(String user, String password) {
+        /*
+            /protocol/openid-connect/token
+         */
+        return null;
+    }
+
+    @Override
+    public boolean isValid(String token) {
+        /*
+            /protocol/openid-connect/token/introspect should be good to check the token
+            requires confidential access type!!!
+         */
+        return false;
+    }
+
+    @Override
+    public String refresh(String token) {
+        /*
+          /protocol/openid-connect/token to use
+         */
+        return null;
+    }
+
+    @Override
+    public boolean logout(String token) {
+        /*
+            /protocol/openid-connect/revoke for token
+            or /protocol/openid-connect/logout
+         */
+        return false;
+    }
 }
